d16433bcdc60146c6ddeac1f80d74883ae3e8499
asasinek123555 <46679532+asasinek123555@users.noreply.github.com>
2023-03-22 14:12:39 +0100

    add example keyboard interface

---
+++ exampleKyboardInterface.py
@@ -0,0 +1,12 @@
+
+class keyboardI:
+    def __init__(self):
+        self.text=""
+    def update(self,img):
+        pass
+    def getText(self):
+        return self.text
+    def addText(self,text):
+        self.test+=text
+    def setText(self,text):
+        self.text = text
